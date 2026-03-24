0d8586c3d4d99e9d985179f4c88d21cd8c049760
Toby Donaldson <tjd@sfu.ca>
2026-03-23 17:05:07 -0700

    updated week 12 notes

--- notes/week12/linked_list1.cpp
+++ notes/week12/linked_list1.cpp
@@ -22,35 +22,65 @@ struct Node
 int main()
 {
     // TODO 1: Make an empty list with no values.
+    Node* head = nullptr;    
 
     // TODO 2: Make a list with one node with data 25.
+    head = new Node{25, nullptr};
 
     // TODO 3: Print the value of the first node.
+    // cout << (*head).data << endl;
+    cout << head->data << endl;
 
     // TODO 4: Add a new value at the front of the list so that it has two
     // values, 10 and then 25.
+    head = new Node{10, head};
 
     // TODO 5: Print the value of the first node.
+    cout << head->data << endl; // 10
 
     // TODO 6: Print the value of the second node.
+    cout << head->next->data << endl; // 25
 
     // TODO 7: Add a new value at the front of the list so that it has three
     // values. 20, 10, and 25
+    head = new Node{20, head};
 
     // TODO 8: Print the first value, second value, and third value of the list.
+    cout << head->data << " "; // 20
+    cout << head->next->data << " "; // 10
+    cout << head->next->next->data << endl; // 25
 
     // TODO 9: Calculate the sum of the values in the list.
+    int a = head->data; // 20
+    int b = head->next->data; // 10
+    int c = head->next->next->data; // 25
+    cout << a + b + c << endl;
 
     // TODO 10: Remove the first value from the list so that the remaining list
     // has two values, 10 and 25. Hint: use a temporary pointer to store the
     // rest of the list.
-
+    // 20 10 25
+    Node* p = head->next; // temp pointer
+    delete head;
+    head = p;
+    
     // TODO 11: Print the first value and second value of the list.
-
+    cout << head->data << " "; // 10
+    cout << head->next->data << endl; // 25
+    
     // TODO 12: Remove the first value of the list so that the main list has
     // just the value 25. Print the resulting list.
+    // 10 25
+    p = head->next; // temp pointer
+    delete head;
+    head = p;
+    cout << head->data << endl; // 25
 
     // TODO 13: Remove the final value of the list. Make sure the list is empty
     // (and there are no memory leaks).
-
+    // 25
+    p = head->next; // temp pointer
+    delete head;
+    head = p;
+    assert(head == nullptr);
 }
--- notes/week12/linked_list2.cpp
+++ notes/week12/linked_list2.cpp
@@ -21,32 +21,79 @@ struct Node
 int main()
 {
     // TODO 1: Make an empty list with no values.
+    Node* head = nullptr; // empty list
 
     // TODO 2: Add the numbers 1, 2, 3, 4 to the list so they are in the order
     // 1, 2, 3, 4.
-
+    head = new Node{4, head};
+    head = new Node{3, head};
+    head = new Node{2, head};
+    head = new Node{1, head};
+        
     // TODO 3: Print the values of the list without using a loop.
+    cout << head->data << " ";
+    cout << head->next->data << " ";
+    cout << head->next->next->data << " ";
+    cout << head->next->next->next->data << endl;;
 
     // TODO 4: Print the values of the list using a while-loop.
+    // start at first node
+    Node* p = head;
+    while (p != nullptr) {
+        cout << p->data << " ";
+        p = p->next; // go to the next node
+    }
+    cout << endl;
 
     // TODO 5: Print the values of the list using a for-loop.
+    for(Node* p = head; p != nullptr; p = p->next) {
+        cout << p->data << " ";
+    }
+    cout << endl;
 
     // TODO 6: Calculate the sum of the values in the list without using a loop
     // and print the result.
+    
+    // you do!
 
     // TODO 7: Calculate the sum of the values in the list using a while-loop
     // and print the result.
+    p = head;
+    int total = 0;
+    while (p != nullptr) {
+        total += p->data;
+        p = p->next; // go to the next node
+    }
+    cout << "sum: " << total << endl; // 10
 
     // TODO 8: Calculate the sum of the values in the list using a for-loop
     // and print the result.
+    total = 0;
+    for(Node* p = head; p != nullptr; p = p->next) {
+        total += p->data;
+    }
+    cout << "sum: " << total << endl; // 10
 
     // TODO 9: Remove each element of the list one at a time, until it is empty,
     // without using a loop.
 
+    
     // TODO 10: Recreate the list so it has the values 1, 2, 3, 4 (in that order).
-
+    
     // TODO 11: Using a while-loop, remove all the values of the list, one
     // removal per iteration. Hint: think of it as repeatedly removing the first
     // value of the list until the list is empty.
-
+    
+    // for(int i = 0; i < 4; ++i) {
+    //     p = head->next; // temp pointer
+    //     delete head;
+    //     head = p;
+    // }
+    p = head;
+    while (p != nullptr) {
+        p = head->next; // temp pointer
+        delete head;
+        head = p;     
+    }
+    assert(p == nullptr);
 }
